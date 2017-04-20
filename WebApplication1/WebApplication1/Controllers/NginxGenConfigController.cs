1bb82497610200f2f1da8c9d68cf5c97f5096fea
DNDXIII <didier.n.dias@gmail.com>
2017-04-20 17:05:14 +0100

    started doing the configuration file

---
+++ WebApplication1/WebApplication1/Controllers/NginxGenConfigController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/generateconfig")]
+    public class NginxGenConfigController:Controller
+    {
+       // private readonly IRepository<Ups> _repository;
+        public NginxGenConfigController() { }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            FileStream f = System.IO.File.Create(@"~/stuff/coise.txt");
+
+            return File(f, f.GetType().ToString());
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/NGINXConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class NGINXConfig
+    {
+        private IDictionary<string, Server> _servers;
+        private IDictionary<string, Upstream> _upstreams;
+        private IDictionary<string, ProxyType> _proxyTypes;
+       // private string pathString = @"../stuff/coise.txt";
+
+        public NGINXConfig(IDictionary<string, Server> servers, IDictionary<string, Upstream> upstreams, IDictionary<string, ProxyType> proxyTypes)
+        {
+            _servers = servers;
+            _upstreams = upstreams;
+            _proxyTypes = proxyTypes;
+        }
+
+        public string GenerateConfig()
+        {
+
+            var strb = new StringBuilder();
+
+
+            foreach (var up in _upstreams.Values)
+            {
+                var i = 0;//to count wich server im on
+                strb.AppendLine("upstream " + up.Name + " {");
+
+                var pType = _proxyTypes[up.ProxyTypeId];
+
+                strb.AppendLine(pType.ProxyValue);
+
+                foreach(var svId in up.ServerIds)
+                {
+                    var sv = _servers[svId];
+                    strb.Append("   server " + sv.Ip + ":" + sv.Port );
+
+                    if (pType.Name == "Weighted Load Balancing")
+                        strb.Append(" weight=" + up.Weights[i]);
+
+                    strb.AppendLine(";");
+                    i++;
+                }
+
+                strb.AppendLine("}");
+            }
+
+            foreach (var server in servers)
+            {
+                strb.AppendLine(server.GenerateConfig());
+            }
+
+            strb.AppendLine(" } ");
+
+            return strb.ToString();
+        }
+
+    }
+}
