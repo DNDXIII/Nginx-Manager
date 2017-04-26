bd9636a7bf0324a0d5158aee67b364fcced6a326
DNDXIII <didier.n.dias@gmail.com>
2017-04-26 14:47:52 +0100

    added allRep file

---
+++ WebApplication1/WebApplication1/DataAccess/AllRepositories.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DataAccess
+{
+    public class AllRepositories
+    {
+        public IRepository<Server> ServerRep { get; }
+        public IRepository<Upstream> UpstreamRep { get; }
+        public IRepository<ProxyType> ProxyTypeRep { get; }
+
+        public AllRepositories(ServersDataAccess s, UpstreamDataAccess u, ProxyTypeDataAccess p)
+        {
+            ServerRep = s;
+            UpstreamRep = u;
+            ProxyTypeRep = p;
+        }
+    }
+}
