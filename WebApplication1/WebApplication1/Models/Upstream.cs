d30c05636427b77e66557e5cd9f2b6dd46372e85
DNDXIII <didier.n.dias@gmail.com>
2017-05-03 14:40:12 +0100

    added maxfails and failtimeout generation

--- WebApplication1/WebApplication1/Models/Upstream.cs
+++ WebApplication1/WebApplication1/Models/Upstream.cs
@@ -12,6 +12,10 @@ namespace WebApplication1.Models
 {
     public class Upstream
     {
+        public const int DEFAULT_MAX_FAILS = 1;
+        public const int DEFAULT_FAIL_TIMEOUT = 10;
+
+
         [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
         public string Id{ get; set; }
         [BsonElement("Name")]
@@ -21,12 +25,11 @@ namespace WebApplication1.Models
         [BsonElement("ServerIds")]
         public List<string> ServerIds { get; set; }
         [BsonElement("MaxFails")]
-        public int MaxFails { get; set; }
+        public int[] MaxFails { get; set; }
         [BsonElement("FailTimeout")]
-        public int FailTimeout { get; set; }
+        public int[] FailTimeout { get; set; }
         [BsonElement("Weights")]
         public int[] Weights { get; set; }
-        
         [BsonElement("FreeText")]
         public string FreeText { get; set; }
         
@@ -34,7 +37,16 @@ namespace WebApplication1.Models
         {
             Weights = new int[] { };
             FreeText="";
+            MaxFails=new int[ServerIds.Count];
+            FailTimeout=new int[ServerIds.Count];
+
+            //to fill the arrays with the default values
+            for(int i =0;i<ServerIds.Count;i++){
+                MaxFails[i]=DEFAULT_MAX_FAILS;
+                FailTimeout[i]=DEFAULT_FAIL_TIMEOUT;
+            }
         }
+
         public string GenerateConfig(AllRepositories allRep)
         {
             var strb = new StringBuilder();
@@ -46,7 +58,16 @@ namespace WebApplication1.Models
 
             for(int i = 0; i < ServerIds.Count; i++)
             {
-                strb.Append(allRep.ServerRep.GetById(ServerIds[i]).GenerateConfig(this, pType.Name, i));
+                strb.Append(allRep.ServerRep.GetById(ServerIds[i]).GenerateConfig());
+                
+                if (pType.ProxyValue == "Weighted Load Balancing")
+                    strb.Append(" weight=" + Weights[i]);
+                if(FailTimeout[i]!=DEFAULT_FAIL_TIMEOUT)
+                    strb.Append(" fail_timeout=" + FailTimeout[i]);
+                if(MaxFails[i]!=DEFAULT_MAX_FAILS)
+                    strb.Append(" max_fails=" + MaxFails[i]);
+
+                 strb.AppendLine(";");
             }
 
             strb.AppendLine(FreeText);
