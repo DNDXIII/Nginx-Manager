2666cdf5a9e9f0f84485aee21a063118270fa2a0
Didier Dias <NB23160@novabase.pt>
2017-05-25 14:05:25 +0100

    included generate config inside the config file, ssh is working

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,18 @@ FROM ubuntu:16.04
 
 RUN \
     apt-get update && \
-    apt-get -yqq install ssh && \
-    apt-get install -y git && \
+    apt-get -y install ssh git libgd3 libxslt1.1 libgeoip1 && \
     mkdir -p /root/.ssh/ && \
     ssh-keyscan gitlab.com >> /root/.ssh/known_hosts && \
     echo '-----BEGIN RSA PRIVATE KEY-----\r\nMIIEpQIBAAKCAQEAxG1H8h7xWsKkuk3xTZ5oojbZo+65KPfP2pvNgMSlntAm2Stv\r\nrux9AlCJwBCawBCGlLvUT9F9rdlhIyasOQWQXpSN1VFZ0Itn5z9LMzStjuvSjAyx\r\n2Y7BVyJ2uw1c5IzXBVD/21qhs32iz+tRAC3oMGzFUVF07R1lsJfSimYTCQEL+2cm\r\nZwZar5BIGaOKQlSYj2FU2QFL0hUhlLojC1RUIjtEgS9qxrjChTdmsdDQ3HztHDzb\r\nrEczm4StUJk2XjgDBPT+rUYDSjYPvGvxqXwc2GgVqD4CkBNVPA9V1eX20yKZtRQr\r\nL4VuiL5WG/eGtWl4FR2LVsP7T2lHCYxYJlEk9wIDAQABAoIBAF15L5qNlQc+xwsN\r\nWj44msxO08UKLjzLqLL8H5AehD4OCd6gp/YS/e4jC1pFzI6HjrOqqn8NTwRzBKGo\r\nnp5IAoqvoMsZemceX+SQnyWP9OOlXz1cMSo5n9HqSZv74O+2AgC6qfblqztyFw9g\r\niNxP/EVHDaMD42OO8LxXk05fAsHuUx3eFscFux3GcxMzOYSOyfT5hL+kY8w6NtbW\r\nljHkggpAbmaFVDvxut7F2ep1XmnrRamtF6BhD8WVJNMYFN7qqe7ctMyLzKiTlnhd\r\nkLDYfZM25aiG1wetc3bKL88qEqViKv53Sd7U0N9Kum3nFf/RcsvRGPO6XWycQrG5\r\nzrm5mSkCgYEA5VoXyTtSWGKwPFH+6YBqOiNSarDaC5wOYY4Pbgu/PUZPxCh/UYAO\r\nQVVoe+HJ7ZhiDoSlFlqCEbM1HF17BK8eF1jc9kDrAJlgU8pBqyixSFSpvDgYAoLs\r\n0UPMP0GeYzH1Kr/JOZAhsjuONQY8kG0CGM3eRmAVQvujr4tI8vwFPGMCgYEA2z/b\r\nP60uPciSx7nEpt3OPfvePYFQPZ7Kvryjfc7w4tO5U0pEhxiRLssdxWdZYnzrPR0w\r\nYbJUa20XzVzWLC0SzhWVu7Il28Vcyf29KKD5/jVQsUIDJnzdZpsW9luXot5/MhhE\r\nXbQeZn8mBMKdUmAU79hXol6/dLz92cTm46vGh10CgYEA3ummrrYSfcdFhQso2Cgw\r\nltsX0oybEKeXrqeVRZ7zO4noIz49KKhusn9hcu5sBZqSy4uOrOBBBRUJrM0KsO3H\r\nMGf1Mv67qbRo1GeDVejSDfwDkOVwKJi5tVbQ4DZVzBGVOPx4hpMYvRN3TwAFw1W/\r\nsP3+aKYgZB8R459bwpbCPAMCgYEAy7JQKKDlTVGDzTuykLWSX9QZblsuCZH2G692\r\n1fPlqVe73r4pXLpeaHMdfcG/MdmNVIJB6QKdnBEHBC00bmaSChXTv0aMFdsPkjrD\r\n+3NTz/IIvibudlub9zAv7UK7Zv4AfrI9EjG97MxErjeBon7R9OOyx4/voK3VyvU8\r\n7lkxYaECgYEAvVUo2fEXhnDwQ3WVh/+ElCEPRspEwcJ7UA6iozoYCgHF+Xe3i0+/\r\nwg/JUN3idEHUTAVXVMa5dqwEPe1ZYE6+R4OEP5J80EfEWUbbsc4vG96aQ6svfDP9\r\nVGuJltxbCcNGiAKsqrFjRjo/0Y0jXGFFyE83hcdYUVfka3UZu+fouUY=\r\n-----END RSA PRIVATE KEY-----' > /root/.ssh/id_rsa && \
     chmod 400 /root/.ssh/id_rsa && \
     git clone git@gitlab.com:collab-azure/nginx-redundant-floatingip-ubuntu.git && \
     cd nginx-redundant-floatingip-ubuntu/debian && \
-    chmod +x *.sh && \
-    ./setup_nginx.sh
+    dpkg -i nginx-common_1.10.0-0ubuntu0.16.04.4-collab_all.deb nginx-full_1.10.0-0ubuntu0.16.04.4-collab_amd64.deb    && \
+                rm /etc/nginx/nginx.conf && \
+                cp nginx.conf /etc/nginx/nginx.conf && \
+                rm /etc/nginx/sites-available/default && \
+                cp default /etc/nginx/sites-available/default && \
+                mkdir -p /etc/nginx/ssl/ && \
+                cp -p  default.crt default.rsa /etc/nginx/ssl/ && \
+                chmod -R 600 /etc/nginx/ssl
--- WebApplication1/WebApplication1/Controllers/TestConfigController.cs
+++ WebApplication1/WebApplication1/Controllers/TestConfigController.cs
@@ -23,6 +23,17 @@ namespace WebApplication1.Controllers
             _allRep = allRep;
         }
 
+        [EnableCors("Cors")]
+        [HttpGet("download")]
+        public void Get()
+        {
+            var config = new NGINXConfig();
+            Response.ContentType = "text/plain";
+            Response.Headers.Append("Content-Description", "File Transfer");
+            Response.Headers.Append("Content-Disposition", "attachment; filename=configuration.conf");
+            Response.WriteAsync(config.GenerateConfig(_allRep));
+        }
+
 
         [EnableCors("Cors")]
         [HttpGet("test")]
@@ -43,8 +54,7 @@ namespace WebApplication1.Controllers
             Process p = new Process();
             ProcessStartInfo pi = new ProcessStartInfo();
             pi.FileName = "cmd.exe";
-            pi.Arguments = @"/C docker run -v " + filePath + @":/etc/nginx/nginx.conf -P -it imkulikov/nginx-sticky nginx -t";
-            Console.WriteLine(pi.Arguments);
+            pi.Arguments = @"/C docker run -v " + filePath + @":/etc/nginx/nginx.conf -P -it nginx-image nginx -t";
             pi.RedirectStandardOutput = true;
             p.StartInfo = pi;
             p.Start();
@@ -65,61 +75,64 @@ namespace WebApplication1.Controllers
         [HttpGet("deploy")]
         public IActionResult DeployConfig()
         {
-            string host = "192.1239.21.2";
-            int port =22;
-            string username= "pi";
-            string password= "ilikepie";
-            string filePath = @"D:\nb23160\Downloads\configurationTest.conf";
-
+            IActionResult ret;
+            string host = "52.233.133.109";
+            int port = 50001;
+            string username = "azureuser";
+            string password = "Collab.1234567890";
+            var filePath = Path.GetTempFileName();
             //create the file
-            System.IO.File.WriteAllText(filePath, _allRep.GeneralConfigRep.GetAll().First().GenerateConfig(_allRep));
 
+
+            //TODO TEMP , ALTERAR !!!!!! apenas pq nao tenho ips como deve ser
+            StringBuilder config = new StringBuilder(_allRep.GeneralConfigRep.GetAll().First().GenerateConfig(_allRep));
+            foreach (var sv in _allRep.ServerRep.GetAll())
+                config.Replace(sv.Address, "127.0.0.1");
+            System.IO.File.WriteAllText(filePath, config.ToString());
+            //TODO TEMP, ALTERAR!!!!
+
+            //System.IO.File.WriteAllText(filePath, _allRep.GeneralConfigRep.GetAll().First().GenerateConfig(_allRep));
 
             //upload the file
             using (var sftp = new SftpClient(host, port, username, password))
             {
-
                 sftp.Connect();
-                sftp.ChangeDirectory("/etc/nginx");
                 using (var uplfileStream = System.IO.File.OpenRead(filePath))
                 {
-                    //rename the previous file
-                    sftp.UploadFile(uplfileStream, filePath, true);
-   
+                    sftp.UploadFile(uplfileStream, "nginxTest.conf", true);
                 }
                 sftp.Disconnect();
-                
+
             }
 
             //test the file again
             using (var sshclient = new SshClient(host, port, username, password))
             {
                 sshclient.Connect();
-                using (var cmd = sshclient.CreateCommand(@"nginx -t -c etc/nginx/nginxTest.conf"))
+                using (var cmd = sshclient.CreateCommand(@"sudo nginx -t -c  /home/azureuser/nginxTest.conf"))
                 {
-                    cmd.Execute();
+                    var s = cmd.Execute();
 
                     //if the test was unsuccessful restore the previous file
                     if (cmd.ExitStatus != 0)
                     {
-                        //remover esta
-                        return BadRequest(cmd.CommandText);
+                        //remove the test file
+                        sshclient.CreateCommand(@"sudo rm /home/azureuser/nginxTest.conf").Execute();
+                        ret = BadRequest(new StreamReader(cmd.ExtendedOutputStream).ReadToEnd());
                     }
+                    //if it was successfull replace the actual file and reload
                     else
                     {
-                        return Ok(cmd.CommandText);
-
+                        ret = Ok(new StreamReader(cmd.ExtendedOutputStream).ReadToEnd());
+                        sshclient.CreateCommand(@"sudo mv /home/azureuser/nginxTest.conf /etc/nginx/nginx.conf").Execute();
+                        var reload = sshclient.CreateCommand(@"sudo systemctl reload nginx");
+                        reload.Execute();
+                        Console.WriteLine(reload.ExitStatus);
                     }
-
-                   
-
                 }
                 sshclient.Disconnect();
             }
-
-            return Ok();
+            return ret;
         }
-        
-
     }
 }
