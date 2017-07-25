f6b1ee4d28a7cf3ecf5a3bf856c485b09d2b9068
unknown <didier.n.dias@gmail.com>
2017-07-25 15:32:21 +0100

    done?

--- upstream_manager/src/Applications.js
+++ upstream_manager/src/Applications.js
@@ -20,8 +20,22 @@ export const ApplicationList = (props) => (
 export const ApplicationEdit = (props) => (
     <Edit title={<EntityName />}  {...props}>
         <SimpleForm >
-            <TextInput source="name" defaultValue="" validate={required} />
-            </SimpleForm>
+            <Field name="name" label="Name" component={TextInput} validate={required} />
+            <Field name="protocol" component={SelectInput} choices={[
+                { id: 'http://', name: 'HTTP' },
+                { id: 'https://', name: 'HTTPS' },
+            ]} optionValue="id" allowEmpty validate={required} label="Protocol" /><br />
+            <ul style={{
+                listStyle: "none",
+                paddingLeft: 13,
+                paddingBottom: 13,
+                paddingTop: 13
+            }}>
+                <h3>Locations</h3>
+                <RenderLocations />
+            </ul>
+
+        </SimpleForm>
     </Edit>
 );
 
@@ -39,52 +53,79 @@ export const ApplicationCreate = (props) => (
                 paddingBottom: 13,
                 paddingTop: 13
             }}>
-
-
                 <h3>Locations</h3>
-
-                <FieldArray name="locations" component={(locations) =>
-                    <div style={{ marginLeft: 13 }}>
-                        {
-                            locations.fields.map((loc, index) =>
-                                <li key={index}>
-                                    <Field
-                                        name={`${loc}.uri`}
-                                        component={TextInput}
-                                        label="URI"
-                                    /><br />
-                                    <Field name={`${loc}.passtype`} component={SelectInput} choices={[
-                                        { id: "proxy_pass" },
-                                        { id: "fastcgi_pass" },
-                                        { id: "uwsgi_pass" },
-                                        { id: "scgi_pass" },
-                                        { id: "memcached_pass" },
-                                    ]} optionText="id" optionValue="id" allowEmpty label="Pass Type" /><br />
-
-
-                                    <Field name={`${loc}.matchType`} component={SelectInput} choices={[
-                                        { id: "=", description: "Exact match (=)" },
-                                        { id: "~", description: "Regex case sensitive (~)" },
-                                        { id: "~*", description: "Regex case insensitive (~*)" },
-                                        { id: "^~", description: "Best non Regex (^~)" },
-                                    ]} optionText="description" optionValue="id" allowEmpty label="Match Type" /><br />
-
-                                    <Field name={`${loc}.freeText`} component={LongTextInput} allowEmpty label="Free Text" /><br />
-
-                                    <RaisedButton secondary={true} style={{ marginTop: 13, marginBottom: 13 }} label="remove" onTouchTap={() => locations.fields.remove(index)} />
-                                    <Divider />
-
-                                </li>
-                            )}
-                        <RaisedButton primary={true} style={{ marginTop: 26, marginBottom: 13, }} label="Add Location" onTouchTap={() => locations.fields.push({})} />
-                    </div>
-
-                } />
+                <RenderLocations />
             </ul>
 
         </SimpleForm>
     </Create>
 );
+
+class RenderLocations extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: [],
+        };
+    }
+
+    render() {
+        return (
+            <FieldArray name="locations" component={(locations) =>
+                <div style={{ marginLeft: 13 }}>
+                    {
+                        locations.fields.map((loc, index) =>
+                            <li key={index}>
+                                <Card style={{ paddingLeft: 13, marginBottom: 13 }} expanded={this.state.expanded[index]} onExpandChange={() => {
+                                    var newExpanded = this.state.expanded;
+                                    newExpanded[index] = !newExpanded[index];
+                                    this.setState({ expanded: newExpanded });
+                                }}>
+                                    <CardHeader title={`${locations.fields.get(index).uri}`} actAsExpander={true} />
+                                    <div expandable={true}>
+                                        <Field
+                                            name={`${loc}.uri`}
+                                            component={TextInput}
+                                            label="URI"
+                                            validate={required}
+                                        /><br />
+                                        <Field name={`${loc}.passtype`} component={SelectInput} choices={[
+                                            { id: "proxy_pass" },
+                                            { id: "fastcgi_pass" },
+                                            { id: "uwsgi_pass" },
+                                            { id: "scgi_pass" },
+                                            { id: "memcached_pass" },
+                                        ]} optionText="id" optionValue="id" allowEmpty label="Pass Type" validate={required} /><br />
+
+
+                                        <Field name={`${loc}.matchType`} component={SelectInput} choices={[
+                                            { id: "=", description: "Exact match (=)" },
+                                            { id: "~", description: "Regex case sensitive (~)" },
+                                            { id: "~*", description: "Regex case insensitive (~*)" },
+                                            { id: "^~", description: "Best non Regex (^~)" },
+                                        ]} optionText="description" optionValue="id" allowEmpty label="Match Type" validate={required} /><br />
+
+                                        <Field name={`${loc}.freeText`} component={LongTextInput} allowEmpty label="Free Text" /><br />
+
+                                        <RaisedButton secondary={true} style={{ marginTop: 13, marginBottom: 13 }} label="remove" onTouchTap={() => {
+                                            locations.fields.remove(index);
+                                            var newExpanded = this.state.expanded;
+                                            newExpanded.pop(index);
+                                            this.setState({ expanded: newExpanded })
+                                        }} />
+                                    </div>
+                                </Card>
+
+                            </li>
+                        )}
+                    <RaisedButton primary={true} style={{ marginTop: 26, marginBottom: 13, }} label="Add Location" onTouchTap={() => locations.fields.push({ uri: ` Location Uri` })} />
+                </div>
+
+            } />
+        )
+    }
+}
+
 
 
 export const ApplicationDelete = (props) => (
